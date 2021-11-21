71275a6b1775fa710d5167094d5707fccf725c74
devshaffan <devshaffan@gmail.com>
2021-11-21 13:55:39 +0500

    Updates

---
+++ precache-manifest.08755ae9a0da2d529b52e9a1918308bd.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a3ea632ba1cbe7d8f0",
+    "url": "/addition_wordProblem/static/css/main.90d44695.chunk.css"
+  },
+  {
+    "revision": "43a3ea632ba1cbe7d8f0",
+    "url": "/addition_wordProblem/static/js/main.0d19b37d.chunk.js"
+  },
+  {
+    "revision": "5cd3b820eb9788e5e09b",
+    "url": "/addition_wordProblem/static/js/runtime~main.e55ee86e.js"
+  },
+  {
+    "revision": "49f3c2ad33750a25c444",
+    "url": "/addition_wordProblem/static/js/2.2e35017f.chunk.js"
+  },
+  {
+    "revision": "f0aa0efad6746287de4c",
+    "url": "/addition_wordProblem/static/js/3.d25448f0.chunk.js"
+  },
+  {
+    "revision": "6fc126f248e4b520c28a6229ca641bc0",
+    "url": "/addition_wordProblem/static/media/mango.6fc126f2.png"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/addition_wordProblem/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "821cde131dca119a05d6f45ed5115aad",
+    "url": "/addition_wordProblem/static/media/grocery.821cde13.gif"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/addition_wordProblem/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/addition_wordProblem/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/addition_wordProblem/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/addition_wordProblem/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/addition_wordProblem/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/addition_wordProblem/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/addition_wordProblem/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/addition_wordProblem/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/addition_wordProblem/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/addition_wordProblem/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/addition_wordProblem/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/addition_wordProblem/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/addition_wordProblem/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/addition_wordProblem/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/addition_wordProblem/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "a0cc197d7b1c5ac20f1a5b0a60cc45ac",
+    "url": "/addition_wordProblem/static/media/typing.a0cc197d.mp3"
+  },
+  {
+    "revision": "d625354e0fe3750134af83e0e4faefd4",
+    "url": "/addition_wordProblem/static/media/apple.d625354e.png"
+  },
+  {
+    "revision": "411c4e6a0a9e83696dc8f57a70f342c0",
+    "url": "/addition_wordProblem/static/media/banana.411c4e6a.png"
+  },
+  {
+    "revision": "091d77928921a2ccefc1456f02198651",
+    "url": "/addition_wordProblem/static/media/orange.091d7792.png"
+  },
+  {
+    "revision": "da95b1d736ed03b78a44d44ef15d813a",
+    "url": "/addition_wordProblem/static/media/pear.da95b1d7.png"
+  },
+  {
+    "revision": "73fe00995cd7adebd696e8d1715500dc",
+    "url": "/addition_wordProblem/static/media/pineapple.73fe0099.png"
+  },
+  {
+    "revision": "5690694919b062e5d201f2895ca093b7",
+    "url": "/addition_wordProblem/static/media/plum.56906949.png"
+  },
+  {
+    "revision": "7b19bf7bd224cdd9941921ee087b2bdd",
+    "url": "/addition_wordProblem/static/media/pomegranate.7b19bf7b.png"
+  },
+  {
+    "revision": "2684d075fc2c72e38524ffc4f7d5df41",
+    "url": "/addition_wordProblem/static/media/strawberry.2684d075.png"
+  },
+  {
+    "revision": "d1d59a88467a6b381df3eee8ce160f25",
+    "url": "/addition_wordProblem/static/media/watermelon.d1d59a88.png"
+  },
+  {
+    "revision": "18ba7a065467efa32ebef20844f445f9",
+    "url": "/addition_wordProblem/static/media/lemon.18ba7a06.png"
+  },
+  {
+    "revision": "7498fd0032f151294c930c93367bd0f9",
+    "url": "/addition_wordProblem/static/media/carrot.7498fd00.png"
+  },
+  {
+    "revision": "560c5f81cbe99a953264d06e0c7cd014",
+    "url": "/addition_wordProblem/static/media/cherry.560c5f81.png"
+  },
+  {
+    "revision": "baee8b37557cf9ba7e6c7016c19d3b06",
+    "url": "/addition_wordProblem/static/media/peach.baee8b37.png"
+  },
+  {
+    "revision": "8506c659726847019116c0159642b02c",
+    "url": "/addition_wordProblem/static/media/dragon_fruit.8506c659.png"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/addition_wordProblem/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "18c8ae84a367ae39c7dc07370d5e133e",
+    "url": "/addition_wordProblem/static/media/paperBag.18c8ae84.png"
+  },
+  {
+    "revision": "e036ffe49c3290b294f2edb25b336a5d",
+    "url": "/addition_wordProblem/static/media/grapes.e036ffe4.png"
+  },
+  {
+    "revision": "f5f258693cd37ccdf82ff06568ee9082",
+    "url": "/addition_wordProblem/static/media/background.f5f25869.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/addition_wordProblem/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/addition_wordProblem/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/addition_wordProblem/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "43d2853e5bd5cc0d947dda76c89c818d",
+    "url": "/addition_wordProblem/index.html"
+  }
+];
